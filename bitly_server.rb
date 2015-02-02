4a62b99a3b0a2a55acac4b089cdc023c3c7c6fb3
Lyra-B <glykeriapeppa@gmail.com>
2015-02-02 21:59:43 +0000

    Bitly Tests Passed

--- bitly_server.rb
+++ bitly_server.rb
@@ -5,9 +5,25 @@ class BitlyServer
 	end
 
 	def add_url(url)
+		arr = [rand(10), rand(10), rand(10)]
+		rand_url = arr.join
+		urls[rand_url] = url
+		rand_url
 	end
 
 	def call(env)
-		[500, {}, 'TODO- make this work']
+		
+		target = env["PATH_INFO"].slice(1, env["PATH_INFO"].length)
+		if urls.include? target
+			[302, {"Location" => urls[target]}, '']
+		elsif env["PATH_INFO"] == '/'
+			[200, {}, 'TODO- make this work']
+		elsif env["QUERY_STRING"] != ''
+			str = env["QUERY_STRING"].gsub(/url=http%3A%2F%2F/, 'http://')
+			
+			[200, {}, add_url(str)]
+		else
+			[404, {}, 'Page not found!']
+		end
 	end
 end
